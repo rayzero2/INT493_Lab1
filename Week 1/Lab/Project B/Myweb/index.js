9c1b0cd4f4f5e9fafee229c3db6f050c6cbdcb3b
Ray <nardrudee156@gmail.com>
2021-01-23 19:59:26 +0700

    Week 1

---
+++ Week 1/Lab/Project B/Myweb/index.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env nodejs
+const bodyParser = require('body-parser')
+const express = require('express')
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+let messages = []
+
+app.get('/', (req, res) => {
+    res.send("hello from index")
+})
+
+app.get('/messages', (req, res) => {
+    res.json({ data: messages })
+})
+//สำหรับ Get 
+
+app.post('/messages', (req, res) => {
+    let inMessage = req.body.text
+    let Found = false
+    if (messages.length > 0) {
+        messages.forEach(message => {
+            if (message.text === inMessage) {
+                message.count = message.count + 1
+                Found = true
+            }
+        })
+        if (!Found) {
+            let data = { text: inMessage, count: 1 }
+            messages.push(data)
+        }
+    } else {
+        let data = { text: inMessage, count: 1 }
+        messages.push(data)
+    }
+
+    res.status(201).json({ text: inMessage })
+})
+//สำหรับ Post 
+
+app.listen(3000, () => {
+    console.log('Start server at port 3000.')
+})
